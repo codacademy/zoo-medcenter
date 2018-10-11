46dfa58642d80eba5b7eaf5b943b3acfde465057
nurgali.yesmukhamedov <nurgali.yesmukhamedov@gmail.com>
2018-10-11 15:59:23 +0600

    Создал проект с помощью start.spring.io добавил проект

---
+++ src/main/java/kz/iitu/projects/zoomedcenter/ZooMedcenterApplication.java
@@ -0,0 +1,12 @@
+package kz.iitu.projects.zoomedcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooMedcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooMedcenterApplication.class, args);
+	}
+}
