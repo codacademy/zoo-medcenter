5fd9941d7db4cf0931722c8c6c09769ab7a01df4
nurgali.yesmukhamedov <nurgali.yesmukhamedov@gmail.com>
2018-10-12 19:00:10 +0600

    Закончил UserRestController

--- src/main/java/kz/iitu/projects/zoomedcenter/model/User.java
+++ src/main/java/kz/iitu/projects/zoomedcenter/model/User.java
@@ -4,10 +4,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Column(name="username")
--- src/main/java/kz/iitu/projects/zoomedcenter/rest/UserRestController.java
+++ src/main/java/kz/iitu/projects/zoomedcenter/rest/UserRestController.java
@@ -3,6 +3,8 @@ package kz.iitu.projects.zoomedcenter.rest;
 import kz.iitu.projects.zoomedcenter.model.User;
 import kz.iitu.projects.zoomedcenter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,6 +25,15 @@ public class UserRestController {
     public ResponseEntity<User> addOwner(@RequestBody @Valid User user, BindingResult bindingResult) throws Exception{
 
         BindingErrorsResponse errors = new BindingErrorsResponse();
+        HttpHeaders headers = new HttpHeaders();
+        if(bindingResult.hasErrors() || (user == null)){
+            errors.addAllErrors(bindingResult);
+            headers.add("errors", errors.toJSON());
+            return new ResponseEntity<User>(user, headers, HttpStatus.BAD_REQUEST);
+        }
+
+        this.userService.saveUser(user);
+        return new ResponseEntity<User>(user, headers, HttpStatus.CREATED);
 
     }
 
